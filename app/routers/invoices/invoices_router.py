fbc84097a4f72322d5c85ee7487792dbc40769a0
Leonel Tapia <leoneltapiad@gmail.com>
2026-09-08 02:52:12 -0600

    fix: hardcode from_email for SendGrid

--- app/routers/invoices/invoices_router.py
+++ app/routers/invoices/invoices_router.py
@@ -939,7 +939,7 @@ async def send_invoice_email(
         invoice_items = db.query(InvoiceItem).filter(InvoiceItem.invoice_id == invoice_id).all()
         company = db.query(Company).first()
 
-        from_email = os.getenv("SENDGRID_FROM_EMAIL")
+        from_email = "powercore.test.email@gmail.com"
 
         html = templates.get_template("invoices/invoice_pdf.html").render(
             {
